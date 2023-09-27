cc155538f7552e878499ec5dc42a6dc7749d945f
weihao <weihaoysgs@gmail.com>
2023-09-27 11:30:52 +0800

    Complete track

--- vins/estimator/feature_tracker.cpp
+++ vins/estimator/feature_tracker.cpp
@@ -10,6 +10,7 @@ FeatureTracker::FeatureTracker()
   MAX_CNT_FEATURES_PER_FRAME = common::Setting::getSingleton()->Get<int>("max_cnt");
   OPTICAL_FLOW_BACK = common::Setting::getSingleton()->Get<int>("flow_back");
   MIN_DIST = common::Setting::getSingleton()->Get<int>("min_dist");
+  SHOW_TRACK_RESULT = common::Setting::getSingleton()->Get<int>("show_track");
   LOG(INFO) << "MAX_CNT_FEATURES_PER_FRAME: " << MAX_CNT_FEATURES_PER_FRAME;
   LOG(INFO) << "OPTICAL_FLOW_BACK: " << OPTICAL_FLOW_BACK;
 }
@@ -37,6 +38,8 @@ FeatureTracker::TrackImage(double current_time, const cv::Mat &img0, const cv::M
     std::vector<float> err;
     cv::calcOpticalFlowPyrLK(
         previous_img_, current_img_, previous_kps_, current_kps_, status, err, cv::Size(21, 21), 3);
+    // size_t track_success_cnt = std::count(status.begin(), status.end(), true);
+    // LOG(INFO) << "Before back track: " << track_success_cnt;
     if (OPTICAL_FLOW_BACK)
     {
       // clang-format off
@@ -47,18 +50,14 @@ FeatureTracker::TrackImage(double current_time, const cv::Mat &img0, const cv::M
                                cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 0.01),
                                cv::OPTFLOW_USE_INITIAL_FLOW);
       // clang-format on
-      for (size_t i = 0; i < status[i]; i++)
+      for (size_t i = 0; i < status.size(); i++)
       {
-        if (status[i] && reverse_status[i] && PtDistance(previous_kps_[i], reverse_pts[i]) <= 0.5)
-        {
-          status[i] = 1;
-        }
-        else
-        {
+        if (!(status[i] && reverse_status[i] && PtDistance(previous_kps_[i], reverse_pts[i]) <= 0.5))
           status[i] = 0;
-        }
       }
     }
+    // track_success_cnt = std::count(status.begin(), status.end(), true);
+    // LOG(INFO) << "After back track: " << track_success_cnt;
     for (int i = 0; i < int(current_kps_.size()); i++)
     {
       if (status[i] && !InBorder(current_kps_[i], current_img_.rows, current_img_.cols))
@@ -93,6 +92,50 @@ FeatureTracker::TrackImage(double current_time, const cv::Mat &img0, const cv::M
     current_kps_.emplace_back(pt);
     track_count_.emplace_back(1);
     ids_.emplace_back(landmark_id_++);
+  }
+
+  /// tracking right image using left image kps
+  {
+    // clang-format off
+    ids_right_.clear();
+    current_right_img_kps_.clear();
+    std::vector<float> err;
+    std::vector<uchar> status;
+
+    cv::calcOpticalFlowPyrLK(current_img_, current_right_img_, current_kps_,
+                             current_right_img_kps_, status, err, cv::Size(21, 21), 3);
+    if (OPTICAL_FLOW_BACK)
+    {
+      std::vector<cv::Point2f> reverse_left_kps;
+      std::vector<float> err_right;
+      std::vector<uchar> status_right;
+      cv::calcOpticalFlowPyrLK(current_right_img_, current_img_, current_right_img_kps_,
+                        reverse_left_kps, status_right, err_right, cv::Size(21, 21), 3);
+      for(size_t i = 0; i < status.size(); i++)
+      {
+          if(!(status[i] && status_right[i] && InBorder(current_right_img_kps_[i], current_right_img_.rows,
+             current_right_img_.cols) && PtDistance(current_kps_[i], reverse_left_kps[i]) <= 0.5))
+            status[i] = 0;
+      }
+    }
+
+    ids_right_ = ids_;
+    ReduceVector(current_right_img_kps_, status);
+    ReduceVector(ids_right_, status);
+    // clang-format on
+  }
+
+  if (SHOW_TRACK_RESULT)
+  {
+    DrawTracker();
+    cv::imshow("draw tracker", draw_track_img_result_);
+    cv::waitKey(1);
+  }
+
+  previous_left_ids_kps_map_.clear();
+  for (size_t i = 0; i < current_kps_.size(); i++)
+  {
+    previous_left_ids_kps_map_[ids_[i]] = current_kps_[i];
   }
 
   previous_img_ = current_img_;
@@ -170,5 +213,39 @@ void FeatureTracker::DrawTrackResultWithLine(int wait_key)
   }
   cv::imshow("track_result", draw_img);
   cv::waitKey(wait_key);
+}
+
+void FeatureTracker::DrawTracker()
+{
+  // clang-format off
+  auto cols = static_cast<float>(current_img_.cols);
+  cv::hconcat(current_img_, current_right_img_, draw_track_img_result_);
+  cv::cvtColor(draw_track_img_result_, draw_track_img_result_, CV_GRAY2RGB);
+  for (size_t j = 0; j < current_kps_.size(); j++)
+  {
+    double len = std::min(1.0, 1.0 * track_count_[j] / 20);
+    /// len = 0 is blue， len = 1 is red
+    cv::circle(draw_track_img_result_, current_kps_[j], 2,
+               cv::Scalar(255 * (1 - len), 0, 255 * len), 2);
+  }
+  for (const auto &kp : current_right_img_kps_)
+  {
+    cv::Point2f right_kp = kp;
+    right_kp.x += cols;
+    cv::circle(draw_track_img_result_, right_kp, 2,
+               cv::Scalar(0, 255, 0), 2);
+  }
+  for (size_t i = 0; i < current_kps_.size(); i++)
+  {
+    auto id = ids_[i];
+    auto iter = previous_left_ids_kps_map_.find(id);
+    if (iter != previous_left_ids_kps_map_.end())
+    {
+      cv::arrowedLine(draw_track_img_result_, current_kps_[i], iter->second,
+                      cv::Scalar(0, 255, 0), 1,
+                      8, 0, 0.2);
+    }
+  }
+  // clang-format on
 }
 } // namespace estimator
--- vins/estimator/feature_tracker.hpp
+++ vins/estimator/feature_tracker.hpp
@@ -18,9 +18,11 @@ public:
   std::map<int, std::vector<std::pair<int, Eigen::Matrix<double, 7, 1>>>>
   TrackImage(double current_time, const cv::Mat &img0, const cv::Mat &img1 = cv::Mat());
   bool InBorder(const cv::Point2f &pt, int row, int col);
+  void DrawTracker();
   void DrawIdsTrackCount(int wait_key);
   void DrawTrackResultWithLine(int wait_key);
   void SetFeatureExtractorMask(cv::Mat &mask);
+  cv::Mat getDrawTrackResultImg() const { return draw_track_img_result_; }
   double PtDistance(cv::Point2f &pt1, cv::Point2f &pt2)
   {
     double dx = pt1.x - pt2.x;
@@ -28,8 +30,7 @@ public:
     return sqrt(dx * dx + dy * dy);
   }
 
-  template <typename T, typename S>
-  void ReduceVector(std::vector<T> &v, const std::vector<S> &status)
+  template <typename T, typename S> void ReduceVector(std::vector<T> &v, const std::vector<S> &status)
   {
     int j = 0;
     for (int i = 0; i < int(v.size()); i++)
@@ -42,14 +43,19 @@ public:
   int OPTICAL_FLOW_BACK;
   int MAX_CNT_FEATURES_PER_FRAME = 0;
   int MIN_DIST; /// min distance between two features
+  int SHOW_TRACK_RESULT;
 
 private:
-  double current_time_;
-  std::vector<int> ids_, ids_right_;
   unsigned int landmark_id_ = 0;
+  double current_time_;
+
+  std::vector<int> ids_, ids_right_;
   std::vector<int> track_count_;
+  std::vector<cv::Point2f> previous_kps_, current_kps_, current_right_img_kps_;
+  std::map<int, cv::Point2f> previous_left_ids_kps_map_;
+
   cv::Mat current_img_, current_right_img_, previous_img_;
-  std::vector<cv::Point2f> previous_kps_, current_kps_, current_right_img_pts_;
+  cv::Mat draw_track_img_result_;
 };
 
 } // namespace estimator
