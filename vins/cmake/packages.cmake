c16bc8560ca8b8686f256e9dd78b39126e7607b0
weihaoysgs <weihao_ysgs@163.com>
2023-10-05 20:15:21 +0800

    Update project cmake sovle ceres config bug

--- vins/cmake/packages.cmake
+++ vins/cmake/packages.cmake
@@ -13,7 +13,7 @@ find_package(Ceres 1.14.0 REQUIRED)
 include_directories(
   ${OpenCV_INCLUDE_DIRS}
   ${catkin_INCLUDE_DIRS}
-  ${Ceres_INCLUDE_DIRS}
+  ${CERES_INCLUDE_DIRS}
   ${PROJECT_SOURCE_DIR}/thirdparty/eigen
   ${PROJECT_SOURCE_DIR}/thirdparty/sophus
   ${PROJECT_SOURCE_DIR}
